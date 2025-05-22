472781f0d5d9081bfbea9fd5597002d009fddc6b
Luis Angel Marin <la.marin@outlook.com>
2025-05-22 13:57:51 -0700

    Rest API Setup

--- src/main/java/com/marin/ProductsService/entities/Product.java
+++ src/main/java/com/marin/ProductsService/entities/Product.java
@@ -1,4 +1,76 @@
 package com.marin.ProductsService.entities;
 
+import jakarta.persistence.*;
+import org.hibernate.id.factory.spi.GenerationTypeStrategy;
+
+@Entity
+@Table(name = "Products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true , nullable = false , length = 128)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(columnDefinition = "INT DEFAULT 0")
+    private int stock;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String description, float price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/main/java/com/marin/ProductsService/service/ProductService.java
+++ src/main/java/com/marin/ProductsService/service/ProductService.java
@@ -1,4 +1,22 @@
 package com.marin.ProductsService.service;
 
-public interface ProducctService {
+import com.marin.ProductsService.entities.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product saveProduct(Product product);
+
+    Product getProductById(int id);
+
+    List<Product> getProducts();
+
+    Product updateProduct(Product product , int id);
+
+    void deleteProduct(int id);
+
+    boolean unstockProduct(int id , int stock);
+
+    boolean stockProduct(int id , int stock);
 }
